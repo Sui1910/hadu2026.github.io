522382c67b196855b23967e0c155909276ab4b43
DuHa <95209381+Sui1910@users.noreply.github.com>
2025-12-31 23:44:42 +0700

    Update start.js

--- start/start.js
+++ start/start.js
@@ -1,5 +1,5 @@
 // ============ COUNTDOWN LOGIC ============
-const targetDate = new Date('2025-01-01T00:00:00').getTime();
+const targetDate = new Date('2025-01-02T00:00:00').getTime();
 const countdownScreen = document.getElementById('countdown-screen');
 const fireworkScreen = document.getElementById('firework-screen');
 const startButton = document.getElementById('start-button');
@@ -508,4 +508,5 @@ function render(speed) {
 
 soundManager.preload();
 handleResize();
+
 
